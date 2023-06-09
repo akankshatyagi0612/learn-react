0ce2b00e0d79ddcbc1d1cde087f438432f3db843
75way05 <75way@75way05s-Air.bbrouter>
2023-06-09 14:25:13 +0530

    Changes

--- src/Components/Expenses/ExpensesFilter.js
+++ src/Components/Expenses/ExpensesFilter.js
@@ -5,16 +5,26 @@ const ExpensesFilter = (props) =>{
     const dropdownChangeHandler = (event) => {
         props.onChangeFilter(event.target.value);
     }
+
+    const itemsYears = props.items.map(item=> {
+        return new Date(item.date).getFullYear()
+    })
+
+    const yearsSet = new Set(itemsYears)
+
+    const years = [...yearsSet]
+
     return(
         <div className={styles['expenses-filter']}>
             <div className={styles['expenses-filter_control']}>
                 <label>Filter By Year</label>
                 <select value={props.selected} onChange={dropdownChangeHandler}>
-                    <option value='2023'>2023</option>
-                    <option value='2022'>2022</option>
+                <option value=''>select year</option>
+                    {years.map(year=> <option key={year} value={year}>{year}</option> )}
+                    {/* <option value='2022'>2022</option>
                     <option value='2021'>2021</option>
                     <option value='2020'>2020</option>
-                    <option value='2019'>2019</option>
+                    <option value='2019'>2019</option> */}
                 </select>
             </div>
         </div>
